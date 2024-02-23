f3b5eb7c224499434b78b97d147d35ad7dbc8afc
Florent MOUGENOT <florent.mougenot@hotmail.fr>
2024-02-23 15:00:43 +0100

    squelette des actions de RideController

--- EVS.Api/Controllers/RideController.cs
+++ EVS.Api/Controllers/RideController.cs
@@ -1,15 +1,82 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using EVS.Core.Models;
 
 namespace EVS.Api.Controllers
 {
     [Route("/ride")]
     [Tags("Trajets")]
+    [ApiController]
     public class RideController : Controller
     {
-        [HttpGet]
-        public IActionResult Index()
+        /// <summary>
+        /// Retourne la liste des trajets correspondants aux 3 critères
+        /// </summary>
+        /// <param name="startCity">Nom de la ville de départ</param>
+        /// <param name="endCity">Nom de la ville de d'arrivée</param>
+        /// <param name="departure">Date de départ</param>
+        [HttpGet("/rides/{startCity}/{endCity}/{departure}")]
+        public IActionResult AllWithEndCity(string startCity, string endCity, DateTime departure)
         {
-            return View();
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Retourne la liste des trajets correspondants aux critères départ/date sans destination précise
+        /// </summary>
+        /// <param name="startCity">Nom de la ville de départ</param>        
+        /// <param name="departure">Date de départ</param>
+        [HttpGet("/rides/{startCity}/{departure}")]
+        public IActionResult AllWithoutEndCity(string startCity, DateTime departure)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Retourne la liste des trajets disponibles sur le site
+        /// </summary>
+        [HttpGet("/rides")]
+        public IActionResult All()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Retourne la liste de tous les trajets d'un utilisateur
+        /// </summary>
+        /// <param name="userId">Identifiant d'utilisateur</param>
+        [HttpGet("/rides/{userId}")]
+        public IActionResult AllByUserId(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Retourne un trajet précis
+        /// </summary>
+        /// <param name="id">Identifiant du trajet</param>
+        [HttpGet("/ride/{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Crée un trajet à proposer sur le site
+        /// </summary>
+        [HttpPost("/create")]
+        public IActionResult Create([FromBody]Ride ride)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Supprime un trajet avec ses commentaires/notes/réservations
+        /// </summary>
+        /// <param name="id">Identifiant du trajet</param>
+        [HttpDelete("/ride/{id}")]
+        public IActionResult DeleteById(Guid id)
+        {
+            throw new NotImplementedException();
         }
     }
 }
