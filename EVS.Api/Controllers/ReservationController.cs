2add845f2345751ea992d16890aca60795c874f5
Florent MOUGENOT <florent.mougenot@hotmail.fr>
2024-02-23 15:01:09 +0100

    squelette des actions de ReservationController

--- EVS.Api/Controllers/ReservationController.cs
+++ EVS.Api/Controllers/ReservationController.cs
@@ -1,15 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EVS.Core.Enums;
+using EVS.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 
 namespace EVS.Api.Controllers
 {
     [Route("/reservation")]
     [Tags("Réservations")]
+    [ApiController]
     public class ReservationController : Controller
     {
-        [HttpGet]
-        public IActionResult Index()
+        /// <summary>
+        /// Crée une réservation pour un passager sur un trajet défini
+        /// </summary>
+        /// <param name="rideId">Identifiant du trajet</param>
+        /// <param name="userId">Identifiant de l'utilisateur</param>
+        [HttpPost("/reservation/{rideId}/{userId}")]
+        public IActionResult Create(Guid rideId, Guid userId)
         {
-            return View();
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Mise à jour d'une réservation (Acception ou Refus des passagers par le conducteur)
+        /// </summary>
+        /// <param name="id">Identifiant de la réservation</param>
+        /// <param name="Status">Identifiant du statut de réservation</param>
+        [HttpPut("/reservation/{id}/{status}")]
+        public IActionResult Update(Guid id, ReservationStatus Status)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Retourne la liste de toutes les réservations concernant un trajet
+        /// </summary>
+        /// <param name="rideId">Identifiant de trajet</param>
+        [HttpGet("/reservations/{rideId}")]
+        public IActionResult AllByRideId(Guid rideId)
+        {
+            throw new NotImplementedException();
         }
     }
 }
