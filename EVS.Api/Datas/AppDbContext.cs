24ac2da6dc113b0319ba90de03cc4b9f204a3106
gmasquelier <gmasquelier@gmail.com>
2024-02-23 14:47:52 +0100

    Ajout des DbSet

--- EVS.Api/Datas/AppDbContext.cs
+++ EVS.Api/Datas/AppDbContext.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EVS.Core.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EVS.Api.Datas
 {
     public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
     {
-
+        DbSet<User> Users { get; set; }
+        DbSet<Feedback> Feedbacks { get; set; }
+        DbSet<Ride> Rides { get; set; }
+        DbSet<Reservation> Reservations { get; set; }
     }
 }
