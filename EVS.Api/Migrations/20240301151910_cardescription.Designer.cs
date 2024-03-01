186716f3038aa6b973c23f218aa447b352eeb00e
Florent MOUGENOT <florent.mougenot@hotmail.fr>
2024-03-01 16:20:44 +0100

    Ajout migrations modif car description

--- EVS.Api/Migrations/20240301151910_cardescription.Designer.cs
+++ EVS.Api/Migrations/20240301151910_cardescription.Designer.cs
@@ -4,6 +4,7 @@ using EVS.Api.Datas;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EVS.Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240301151910_cardescription")]
+    partial class cardescription
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EVS.Api/Migrations/20240301151910_cardescription.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EVS.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class cardescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "user",
+                keyColumn: "id",
+                keyValue: new Guid("3a408642-5cc6-436d-bb51-1fb3bb537065"));
+
+            migrationBuilder.InsertData(
+                table: "user",
+                columns: new[] { "id", "birthday", "car_description", "email", "firstname", "lastname", "password", "phone_number", "photo", "pseudo" },
+                values: new object[] { new Guid("54823d83-934f-4735-b4ea-06d9b3a283ac"), new DateTime(2024, 3, 1, 16, 19, 10, 3, DateTimeKind.Local).AddTicks(3702), "", "admin@envoituresimone.com", "Admin", "Admin", "123456789", "0102030405", "", "admin" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "user",
+                keyColumn: "id",
+                keyValue: new Guid("54823d83-934f-4735-b4ea-06d9b3a283ac"));
+
+            migrationBuilder.InsertData(
+                table: "user",
+                columns: new[] { "id", "birthday", "car_description", "email", "firstname", "lastname", "password", "phone_number", "photo", "pseudo" },
+                values: new object[] { new Guid("3a408642-5cc6-436d-bb51-1fb3bb537065"), new DateTime(2024, 3, 1, 13, 56, 52, 318, DateTimeKind.Local).AddTicks(8560), "", "admin@envoituresimone.com", "Admin", "Admin", "123456789", "0102030405", "", "admin" });
+        }
+    }
+}
