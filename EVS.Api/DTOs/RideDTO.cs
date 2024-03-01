4075d7c40301b1f7e186ae21400838aed775c87d
Florent MOUGENOT <florent.mougenot@hotmail.fr>
2024-03-01 11:01:45 +0100

    Ajout de UserDTO et RideDTO

---
+++ EVS.Api/DTOs/RideDTO.cs
@@ -0,0 +1,31 @@
+﻿using EVS.Core.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EVS.Api.DTOs
+{
+    public class RideDTO
+    {
+        public Guid Id { get; set; }
+        [Required, MaxLength(50)]
+        public string? StartCity { get; set; }
+        [Required]
+        public Double StartLatitude { get; set; }
+        [Required]
+        public Double StartLongitude { get; set; }
+        [Required, MaxLength(50)]
+        public string? EndCity { get; set; }
+        [Required]
+        public Double EndLatitude { get; set; }
+        [Required]
+        public Double EndLongitude { get; set; }
+        [Required]
+        public int Distance { get; set; }
+        [Required]
+        public DateTime Departure {  get; set; }
+        [Required]
+        public Double Price { get; set; }
+        [Required, Range (1, 10)]
+        public int Seats {  get; set; }
+    }
+}
